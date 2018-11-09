552d7efd7dc4fb21bbdbcc9896069df5987747f0
yuyunchao <294733692@qq.com>
2018-11-09 15:06:20 +0800

    电影评分组件

--- src/routes/detail/index.jsx
+++ src/routes/detail/index.jsx
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import BaseInfo from './components/BaseInfo';
+import ScoreSummary from './components/ScoreSummary';
 import './index.css';
 
 class Detail extends Component {
   render() {
     return (
-      <div>
         <div className="detail">
           <div className="detail__top">
             <div className="tOperator">
@@ -15,8 +15,12 @@ class Detail extends Component {
             </div>
             <BaseInfo />
           </div>
+          <div className="detail__content">
+            <div className="detail__module">
+              <ScoreSummary />
+            </div>
+          </div>
         </div>
-      </div>
     );
   }
 }
