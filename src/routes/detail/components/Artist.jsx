d76c23117989442124822a2638e20125fb6158c4
yuyunchao <294733692@qq.com>
2018-12-17 15:36:12 +0800

    演职人员组件

---
+++ src/routes/detail/components/Artist.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Artist.css';
+
+/**
+ * @desc 详情页演职员表
+ * @param props
+ * @returns {*}
+ * @constructor
+ */
+
+const Artist = props => {
+  return (
+    <div className="mArtist">
+      <ul className="mArtist__list">
+        <li className="">
+          <a href="xxxx" className="artistInfo">
+            <div className="artistInfo__image" style={{ backgroundImage: "url(xxx.jpeg)" }} />
+            <div>
+              <dl className="artistInfo__name">张艺谋</dl>
+              <dd className="artistInfo__job">导演</dd>
+            </div>
+          </a>
+        </li>
+        <li className="">
+          <a href="xxxx" className="artistInfo">
+            <div className="artistInfo__image" style={{ backgroundImage: "url(xxx.jpeg)" }} />
+            <div>
+              <dl className="artistInfo__name">张艺谋</dl>
+              <dd className="artistInfo__job">导演</dd>
+            </div>
+          </a>
+        </li>
+        <li className="">
+          <a href="xxxx" className="artistInfo">
+            <div className="artistInfo__image" style={{ backgroundImage: "url(xxx.jpeg)" }} />
+            <div>
+              <dl className="artistInfo__name">张艺谋</dl>
+              <dd className="artistInfo__job">导演</dd>
+            </div>
+          </a>
+        </li>
+        <li className="">
+          <a href="xxxx" className="artistInfo">
+            <div className="artistInfo__image" style={{ backgroundImage: "url(xxx.jpeg)" }} />
+            <div>
+              <dl className="artistInfo__name">张艺谋</dl>
+              <dd className="artistInfo__job">导演</dd>
+            </div>
+          </a>
+        </li>
+        <li className="">
+          <a href="xxxx" className="artistInfo">
+            <div className="artistInfo__image" style={{ backgroundImage: "url(xxx.jpeg)" }} />
+            <div>
+              <dl className="artistInfo__name">张艺谋</dl>
+              <dd className="artistInfo__job">导演</dd>
+            </div>
+          </a>
+        </li>
+        <li className="">
+          <a href="xxxx" className="artistInfo">
+            <div className="artistInfo__image" style={{ backgroundImage: "url(xxx.jpeg)" }} />
+            <div>
+              <dl className="artistInfo__name">张艺谋</dl>
+              <dd className="artistInfo__job">导演</dd>
+            </div>
+          </a>
+        </li>
+        <li className="">
+          <a href="xxxx" className="artistInfo">
+            <div className="artistInfo__image" style={{ backgroundImage: "url(xxx.jpeg)" }} />
+            <div>
+              <dl className="artistInfo__name">张艺谋</dl>
+              <dd className="artistInfo__job">导演</dd>
+            </div>
+          </a>
+        </li>
+        <li className="">
+          <a href="xxxx" className="artistInfo">
+            <div className="artistInfo__image" style={{ backgroundImage: "url(xxx.jpeg)" }} />
+            <div>
+              <dl className="artistInfo__name">张艺谋</dl>
+              <dd className="artistInfo__job">导演</dd>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Artist.propTypes = {
+
+};
+
+export default Artist;
--- src/routes/detail/index.jsx
+++ src/routes/detail/index.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import BaseInfo from './components/BaseInfo';
 import ScoreSummary from './components/ScoreSummary';
 import CollapsibleText from '../../components/CollapsibleText';
+import Artist from './components/Artist';
 import './index.css';
 
 class Detail extends Component {
@@ -26,6 +27,10 @@ class Detail extends Component {
                   天赋异禀的结巴少年秦风（刘昊然 饰）警校落榜，被姥姥遣送泰国找远房表舅——号称“唐人街第一神探”，实则猥琐的大叔唐仁（王宝强 饰）散心。不想一夜花天酒地后，唐仁沦为离奇凶案嫌疑人，不得不和秦风亡命天涯……穷追不舍的警探黄兰登（陈赫 饰）、无敌幸运的警察坤泰（肖央 饰）、穷凶极恶的匪帮三人组、高深莫测的唐人街教父、美艳风骚的老板娘阿香（佟丽娅 饰）等悉数登场！
                 </div>
               </CollapsibleText>
+            </div>
+            <div className="detail__module">
+              <h3 className="detail__moduleTitle">演职人员</h3>
+              <Artist/>
             </div>
           </div>
         </div>
