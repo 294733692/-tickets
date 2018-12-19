d27bf6f9865c4f166fb510ab1ea65510d2b040bc
yuyunchao <294733692@qq.com>
2018-12-19 10:28:07 +0800

    评论标签列表

--- src/routes/detail/container/Comment.jsx
+++ src/routes/detail/container/Comment.jsx
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import ScoreDistribute from '../components/ScoreDistribute';
+import TagList from '../components/TagList';
 
 class Comment extends Component {
   render() {
     return (
       <div className='mComment'>
         <ScoreDistribute />
+        <div style={{ marginTop: 16}}>
+          <TagList />
+        </div>
       </div>
     );
   }
